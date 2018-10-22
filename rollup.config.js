06eec0519a785d47d6d22817351fc72ab3ddf32d
yidafu <tiaozuwushi@qq.com>
2018-10-22 22:06:19 +0800

    build(rollup.config.js package.json yarn.lock lib/index.js): compress code. output file with ESModul
    rollup-plugin-uglify isn't able to transpile es6 syntax, so I replace uglify with terser. the output
    file shoud be ESModules, not commondJS.

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,30 @@
-import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
-import json from "rollup-plugin-json";
 import {
-  uglify
-} from "rollup-plugin-uglify";
+  terser
+}
+from "rollup-plugin-terser";
 import filesize from 'rollup-plugin-filesize'
 
 export default {
   input: './src/index.js',
   output: {
     file: './lib/index.js',
-    format: 'cjs'
+    format: 'esm'
   },
   plugins: [
-    // json(),
-    resolve(),
     babel({
       exclude: "node_modules/**",
       runtimeHelpers: true
     }),
     commonjs(),
-    // uglify(),
     filesize(),
+    terser({
+      sourcemap: true,
+      output: {
+        comments: false,
+        indent_level:2
+      }
+    }),
   ],
 };
