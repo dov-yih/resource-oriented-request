04112306231626210e7e41ac78fa83b29b37501f
yidafu <1427019344@qq.com>
2018-10-30 01:05:01 +0800

    test(test/index.test.js): test for static header property.
    #4

--- test/index.test.js
+++ test/index.test.js
@@ -3,14 +3,37 @@ const assert = require('power-assert')
 // import API, { baseURL} from '../src'
 const {API, baseURL} = require('../lib')
 describe('@baseURL should not cover the old',() => {
-  it('prefix', () => {
-    @baseURL('http://localhost:3000')
-    class Base extends API {}
-    class Test extends Base {}
-    class Test2 extends Base {
-      static prefix = 'v2'
+  @baseURL('http://localhost:3000')
+  class Base extends API {}
+  it('prefix can\'t overwrite:', () => {
+    class User extends Base {}
+    class Post extends Base {
+      static prefix = 'admin'
     }
-    assert.equal(Test.pathname, 'test')
-    assert.equal(Test2.pathname, 'v2/test2')
+    assert.equal(User.pathname(), 'users')
+    assert.equal(User.pathname(11), 'users/11')
+    assert.equal(Post.pathname(), 'admin/posts')
+    assert.equal(Post.pathname(1), 'admin/posts/1')
+  })
+  it('headers can\'t overwrite:',() => {
+    // class
+    class User extends Base {
+      static headers = {
+        'X-Tag': 'test-User'
+      }
+    }
+    assert.deepEqual(User.headers, {
+      'X-Tag': 'test-User'
+    })
+    class Post extends Base {
+      static headers = {
+        'X-Tag': 'test-Post',
+        'X-AXIOS': 'Static'
+      }
+    }
+    assert.deepEqual(Post.headers, {
+      'X-Tag': 'test-Post',
+      'X-AXIOS': 'Static'
+    })
   })
 })
